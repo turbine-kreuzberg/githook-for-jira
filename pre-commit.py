ac144ee99e4fb86e396d39016f4227a990e26829
raymond schmidt <raymond.schmidt@votum.de>
2016-02-25 20:57:48 +0000

    BIO-25

--- pre-commit.py
+++ pre-commit.py
@@ -8,22 +8,40 @@ project_format = '[A-Z][A-Z]+'
 issue_pattern = '{}-[\d]+'.format(project_format)
 
 import sys, os, re, git, getpass
+import requests
+import json
 #from subprocess import check_output
 
 #teststring = "#muh"
+
+
+def build_commit_json(message):
+	return "{\"body\": \"%s\"}" % (message)
+
+def send_commit_message_to_jira(url, ticket, message, paswd):
+	headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+	r = requests.post(url, data=build_commit_json(message), headers=headers auth=(get_user(), paswd))
+	print r
+
+def get_user():
+	return g.config('--get','user.email')
+
+
 g = git.Git('.')
-ticket=g.log('-1', '--pretty=%s')
+ticket = g.log('-1', '--pretty=%s')
 ticket = re.search(issue_pattern,ticket)
+message = g.log('-1', '--pretty=%b')
+url = 'https://votum-projects.atlassian.net/rest/api/2/issue/'
 
 username = g.config('--get','user.email')
-
+print message
 print 'username from git %s' % (username)
 paswd = getpass.getpass('password: ')
 
 if ticket:
-    "muh %s\n" % (ticket.group(0))
+	send_commit_message_to_jira (url, ticket.group(0), message, paswd)
 
 else:
     print "no ticket id found"
 
-#curl -D- -u username:password -X POST --data "{\"body\": \"$message\"}" -H "Content-Type: application/json" https://votum-projects.atlassian.net/rest/api/2/issue/
+#curl -D- -u username:password -X POST --data "{\"body\": \"$message\"}" -H "Content-Type: application/json" 
