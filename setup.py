971f7a5ec6eeac2e6c2a3f605311eb0ae0c253c7
raymond.schmidt <raymond.schmidt@votum.de>
2016-03-01 18:24:39 +0100

    changed setup.py removed readme

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+#with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+#    long_description = f.read()
 
 setup(
     name='git hook for jira',
