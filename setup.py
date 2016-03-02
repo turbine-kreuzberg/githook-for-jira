1d25fbaa6f9a0ac7b8447d1af0f50cf0688a9e84
raymond.schmidt <raymond.schmidt@votum.de>
2016-03-02 16:17:11 +0100

    - update requirements

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.3',
+    version='0.0.4',
 
     description='commit message will send to jira as comment',
     long_description='''
@@ -70,15 +70,21 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    #install_requires=['peppercorn'],
+    install_requires=[
+        'setuptools',
+        'requests',
+        'GitPython',
+
+    ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
+
     extras_require={
         'dev': ['check-manifest'],
-        'test': ['coverage'],
+        'test': ['coverage', 'mock'],
     },
 
     # If there are data files included in your packages that need to be
